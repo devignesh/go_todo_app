eeeef9b8633379f43212a8e5ddc0c9bc6eba451d
devignesh <vinesh1865@gmail.com>
2019-08-15 00:29:03 +0530

    adding scripting code

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+// import the Container Component from the semantic-ui-react
+import { Container } from "semantic-ui-react";
+// import the ToDoList component
+import ToDoList from "./To-Do-List";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <ToDoList />
+      </Container>
     </div>
   );
 }
-
 export default App;
